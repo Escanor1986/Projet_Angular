f5b46c3bf96f2605b6cd6544d5c571ababc8d0ab
Escanor1986 <lionel.zovi@gmail.com>
2023-12-14 21:28:54 +0100

    added heroes component, hero interface, mock heroes, app module & some style to the app, style got issue with testing only with workflow, not on local test

--- projet-angular/src/app/app.component.spec.ts
+++ projet-angular/src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'projet-angular' title`, () => {
+  it(`should have the 'Tour of Heroes' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projet-angular');
+    expect(app.title).toEqual('Tour of Heroes');
   });
 
   it('should render title', () => {
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, projet-angular'
+      'Tour of Heroes'
     );
   });
 });
