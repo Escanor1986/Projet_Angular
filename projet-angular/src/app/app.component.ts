6cd1ff6c8d154a5d7e9c2acdfda8203f3211d737
Escanor1986 <lionel.zovi@gmail.com>
2023-12-15 17:41:49 +0100

    added services && messageComponent refactored data access to the HeroService class. registered the HeroService as the provider of its service at the root level so that it can be injected anywhere in the application. used Angular Dependency Injection to inject it into a component. gave the HeroService get data method an asynchronous signature. discovered Observable and the RxJS Observable library. used RxJS of() to return Observable<Hero[]>, an observable of mock heroes. component's ngOnInit lifecycle hook calls the HeroService method, not the constructor. created a MessageService for loosely coupled communication between classes. HeroService injected into a component is created with another injected service, MessageService.

--- projet-angular/src/app/app.component.ts
+++ projet-angular/src/app/app.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule, UpperCasePipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroesComponent, UpperCasePipe],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeroesComponent,
+    UpperCasePipe,
+    MessagesComponent,
+  ],
 })
 export class AppComponent {
   title = 'Tour of Heroes';
--- projet-angular/src/app/heroes/heroes.component.ts
+++ projet-angular/src/app/heroes/heroes.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UpperCasePipe, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
@@ -13,11 +14,26 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
+
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 }
