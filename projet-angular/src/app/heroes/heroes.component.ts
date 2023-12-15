a9522edb0016810c414e073ad9e9a313480063e0
Escanor1986 <lionel.zovi@gmail.com>
2023-12-15 14:50:58 +0100

    created seprate & reusable herodetailcomponent, property binding bewteen parent-heroescomponent & child-herodetailcomponent, use of @inout decorator to make hero propoerty available for binding

--- projet-angular/src/app/heroes/heroes.component.ts
+++ projet-angular/src/app/heroes/heroes.component.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule, NgFor, NgIf], // placer les "Pipes" dans les imports en mode "standalone"
+  imports: [UpperCasePipe, FormsModule, NgFor, NgIf, HeroDetailComponent], // placer les "Pipes" dans les imports en mode "standalone"
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.scss',
+  styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent {
   heroes = HEROES;
